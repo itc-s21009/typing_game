b93de7c94a04a6fdb5c18801362ea0f2785a86c5
itc-s21009 <s21009@std.it-college.ac.jp>
2022-12-08 14:41:59 +0900

    ボタン見た目だけ

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
 const [width, height] = [800, 500]
 
+class CustomButton extends Phaser.GameObjects.Container {
+    text
+    container
+
+    constructor(scene, x, y, W, H, text, onClick) {
+        super(scene, x, y)
+        const fontSize = 32
+        const colors = {
+            container: {
+                out: 0x808080,
+                hover: 0x202020
+            },
+            text: {
+                // out: 0x000000,
+                // hover: 0xffffff
+                out: 'black',
+                hover: 'white'
+            }
+        }
+        this.scene = scene
+        this.scene.add.existing(this)
+        this.setSize(W, H)
+        this.setInteractive({useHandCursor: true})
+        this.text = scene.add.text(0, -1, text,
+            {fontSize: fontSize, color: colors.text.out}).setOrigin(0.5, 0.5)
+        this.container = scene.add.rectangle(0, 0, W, H)
+        this.container.setStrokeStyle(0, 0xffffff)
+        this.container.setFillStyle(colors.container.out)
+        this.add([this.container, this.text])
+        this.on('pointerover', () => {
+            this.container.setFillStyle(colors.container.hover)
+            this.text.setColor(colors.text.hover)
+        })
+        this.on('pointerout', () => {
+            this.container.setFillStyle(colors.container.out)
+            this.text.setColor(colors.text.out)
+        })
+        this.on('pointerup', onClick)
+    }
+}
+
 class SceneTitle extends Phaser.Scene {
     constructor() {
         super();
@@ -10,8 +51,17 @@ class SceneTitle extends Phaser.Scene {
             .setFontSize(100)
             .setColor('#000')
             .setOrigin()
+        this.add.existing(new CustomButton(this, width / 4, 300, 250, 100, 'スタート', () => {
+                console.log("start")
+            }
+        ))
+        this.add.existing(new CustomButton(this, width / 4 * 3, 300, 250, 100, 'ランキング', () => {
+                console.log("ranking")
+            }
+        ))
     }
 }
+
 const config = {
     type: Phaser.AUTO,
     width: width,
