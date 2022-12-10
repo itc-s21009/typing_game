45d1e98da157b3e25b6bf8e385bdd106a4577660
itc-s21009 <s21009@std.it-college.ac.jp>
2022-12-10 13:00:04 +0900

    少し修正

--- src/custom_text.js
+++ src/custom_text.js
@@ -13,7 +13,11 @@ export default class CustomText extends Phaser.GameObjects.Text {
      * @param align
      * @returns {CustomText}
      */
-    setAlignCenterHorizontally = (align) => this.setOrigin(align ? 0.5 : 1, this.originY)
+    setAlignCenterHorizontally = (align) => {
+        this.originX = align ? 0.5 : 1
+        this.updateDisplayOrigin()
+        return this
+    }
 
     /**
      * 文字を縦方向に中央揃えするかを設定する
@@ -21,5 +25,17 @@ export default class CustomText extends Phaser.GameObjects.Text {
      * @param align
      * @returns {CustomText}
      */
-    setAlignCenterVertically = (align) => this.setOrigin(this.originX, align ? 0.5 : 1)
+    setAlignCenterVertically = (align) => {
+        this.originY = align ? 0.5 : 1
+        this.updateDisplayOrigin()
+        return this
+    }
+
+    /**
+     * 文字を上下左右方向に中央揃えするかを設定する
+     * alignがtrueの場合は中央揃えにする
+     * @param align
+     * @returns {CustomText}
+     */
+    setCentered = (align) => this.setOrigin(align ? 0.5 : 1, align ? 0.5 : 1)
 }
--- src/scenes/scene_title.js
+++ src/scenes/scene_title.js
@@ -1,7 +1,7 @@
 import CustomButton from "../custom_button.js";
 import {SCENE_PLAY, SCENE_RANKING, SCENE_TITLE, WIDTH} from "./scene_loader.js";
 import CustomText from "../custom_text.js";
-import {DIFFICULTY} from "./scene_play";
+import {DIFFICULTY} from "./scene_play.js";
 
 export class SceneTitle extends Phaser.Scene {
     constructor() {
@@ -11,7 +11,7 @@ export class SceneTitle extends Phaser.Scene {
     create() {
         this.add.existing(new CustomText(this, WIDTH / 2, 100, 'タイピング').setAlignCenterHorizontally(true))
         this.add.existing(new CustomButton(this, WIDTH / 4, 300, 250, 100, 'スタート', () => {
-            this.scene.start(SCENE_PLAY, {difficulty: DIFFICULTY.HARD})
+            this.scene.start(SCENE_PLAY, {difficulty: DIFFICULTY.NORMAL})
         }))
         this.add.existing(new CustomButton(this, WIDTH / 4 * 3, 300, 250, 100, 'ランキング', () => {
             this.scene.start(SCENE_RANKING)
