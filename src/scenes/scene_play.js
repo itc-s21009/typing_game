b6640fe4939726bea5c9bbada1d6798b0bc0c179
itc-s21009 <s21009@std.it-college.ac.jp>
2022-12-11 11:31:13 +0900

    拗音に対応

--- src/scenes/scene_play.js
+++ src/scenes/scene_play.js
@@ -202,13 +202,17 @@ const easy = [
 const normal = [
     // {display: '吾輩は猫である', kana: 'わがはいはねこである'},
     // {display: 'お寿司が食べたい', kana: 'おすしがたべたい'},
-    {display: 'タイピングゲーム', kana: 'たいぴんぐげーむ'},
+    // {display: 'タイピングゲーム', kana: 'たいぴんぐげーむ'},
+    // {display: '朱色', kana: 'しゅいろ'},
     // {display: '橋本環奈', kana: 'はしもとかんな'},
-    {display: 'ピッタンコ', kana: 'ぴったんこ'},
-    {display: 'あっいっう', kana: 'あっいっう'},
+    // {display: 'ピッタンコ', kana: 'ぴったんこ'},
+    // {display: 'あっいっう', kana: 'あっいっう'},
     // {display: '星のカービィ', kana: 'ほしのかーびぃ'},
     // {display: '名前はまだ無い', kana: 'なまえはまだない'},
-    {display: 'スマッシュ', kana: 'すまっしゅ'},
+    // {display: 'スマッシュ', kana: 'すまっしゅ'},
+    {display: 'やっふー', kana: 'やっふー'},
+    {display: 'あっつい', kana: 'あっつい'},
+    {display: 'ふぉっふぉっふぉ', kana: 'ふぉっふぉっふぉ'},
     // {display: 'プログラミング', kana: 'ぷろぐらみんぐ'},
 ]
 const hard = [
@@ -244,39 +248,41 @@ export class ScenePlay extends Phaser.Scene {
                 .setAlign('center'))
             return
         }
+        // 「しゅ」とかのローマ字一覧に「し」と「ゅ」のローマ字一覧を全部まとめる
+        // 「しゅ」の場合は「し」に3個、「ゅ」に2個、「しゅ」に元々2個候補があるので、
+        // 3x2+2 で最終的に「しゅ」の要素数は8になるはず
+        // 'し': [ 'si', 'shi', 'ci' ],
+        // 'ゅ': [ 'lyu', 'xyu' ],
+        // 'しゅ': [
+        //   'syu',    'shu',
+        //   'silyu',  'sixyu',
+        //   'shilyu', 'shixyu',
+        //   'cilyu',  'cixyu'
+        // ]
+        const prepareForSmallChars = () => Object.keys(romans).filter(k => k.length === 2).forEach(str => {
+            const chars = str.split('')
+            const [romanList1, romanList2] = [romans[chars[0]], romans[chars[1]]]
+            const condidatesToAdd = romanList1.map(x => romanList2.map(y => `${x}${y}`)).flat()
+            romans[str].push(...condidatesToAdd)
+        })
         const kanaToRoman = (kana) => kana.split('').map(k => romans[k][0]).join('')
         const sentences = SENTENCES[difficulty]
-        let sentence, kanaRomanMap, kanaIndex, romanInput
-        this.input.keyboard.on('keydown', (e) => {
-            let d = kanaRomanMap[kanaIndex]
-            // 今打ってる文字が最後の文字じゃない場合
-            if (kanaIndex < kanaRomanMap.length - 1) {
-                const nextChar = kanaRomanMap[kanaIndex + 1]
-                console.log(nextChar)
-                const nextRomanChar = nextChar.roman[0][0]
-                // 今打ってる文字が「ん」で、「n」まで打ってる時
-                const condN = d.kana === 'ん' && romanInput === 'n'
-                // 今打ってる文字が「っ」で、次の文字が母音じゃない場合
-                const condT = d.kana === 'っ' && !'あいうえお'.split('').includes(nextChar.kana)
-                if (condN || condT) {
-                    // 次の文字の最初のローマ字がn以外で、それが打ったキーと一致した場合
-                    if (nextRomanChar !== 'n' && nextRomanChar === e.key) {
-                        kanaIndex++
-                        romanInput = ''
-                        if (condN) {
-                            // 「ん」の判定の場合は、次の文字の判定をこの先で実行する
-                            d = kanaRomanMap[kanaIndex]
-                        } else if (condT) {
-                            // 「っ」の判定の場合は、一旦処理を止める
-                            return
-                        }
-                    }
-                }
-            }
-            const candidates = d.roman.filter(r => r.startsWith(`${romanInput}${e.key}`))
+        let sentence            // 出題中の文章
+        // 出題中の文章の、かなとローマ字の連想配列のリスト
+        // [
+        //   {kana: 'か', roman: ['ka', 'ca']}
+        //   {kana: 'な', roman: ['na']},
+        // ]
+        // のように並ぶ
+        let kanaRomanMap
+        let kanaIndex           // 今打ってる文字の位置
+        let romanInput          // 今打ってる文字に対して、どんなキーで正しく打ったか
+        let inputForSentence    // 出題中の文章に対して、どんなキーで正しく打ったか
+        const checkByCandidates = (candidates, key) => {
             if (candidates.length > 0) {
-                romanInput += e.key
-                console.log(romanInput, candidates)
+                romanInput += key
+                inputForSentence += key
+                console.log(romanInput, candidates, inputForSentence)
                 this.text_roman.text = `${romanInput === candidates[0] ? '' : candidates[0].slice(romanInput.length)}${kanaRomanMap.map(d => d.roman[0]).slice(kanaIndex + 1).join('')}`
                 if (romanInput === candidates[0]) {
                     kanaIndex++
@@ -285,15 +291,64 @@ export class ScenePlay extends Phaser.Scene {
                 }
                 if (kanaIndex === kanaRomanMap.length) {
                     kanaIndex = 0
+                    inputForSentence = ''
                     showRandomSentence()
                 }
             }
+        }
+        this.input.keyboard.on('keydown', (e) => {
+            let d = kanaRomanMap[kanaIndex]
+            // 今打ってる文字が最後の文字じゃない場合
+            if (kanaIndex < kanaRomanMap.length - 1) {
+                const nextChar = kanaRomanMap[kanaIndex + 1]
+                const nextRomanChar = nextChar.roman[0][0]
+                // 今打ってる文字が「ん」で、「n」まで打ってる時
+                const condN = d.kana === 'ん' && romanInput === 'n'
+                // 今打ってる文字が「っ」で、次の文字が母音じゃない場合
+                const condT = d.kana === 'っ' && !'あいうえお'.split('').includes(nextChar.kana)
+                // 次の文字が「っ」以外の小文字の場合
+                const condY = 'ぁぃぅぇぉゃゅょ'.includes(nextChar.kana)
+                if (condN || condT) {
+                    // 次の文字の最初のローマ字がn以外で、それが打ったキーと一致した場合
+                    if (nextRomanChar !== 'n' && nextChar.roman.filter(r => r.startsWith(e.key)).length > 0) {
+                        kanaIndex++
+                        romanInput = ''
+                        if (condN) {
+                            // 「ん」の判定の場合は、次の文字の判定をこの先で実行する
+                            d = kanaRomanMap[kanaIndex]
+                        } else if (condT) {
+                            // 「っ」の判定の場合は、一旦処理を止める
+                            inputForSentence += e.key
+                            return
+                        }
+                    }
+                } else if (condY) {
+                    // 「ち」+「ゃ」のように連結して、存在するパターンかをチェックする
+                    const candidateKana = `${d.kana}${nextChar.kana}`
+                    const candidateKanaData = romans[candidateKana]
+                    // パターンが存在した場合
+                    if (candidateKanaData !== undefined) {
+                        // 「ち」の次に「ゃ」が来るような場合は、
+                        // 「ちゃ」の１文字として処理する
+                        // ['ち', 'ゃ', 'い', 'ろ'] の場合は、
+                        // ['ちゃ', 'い', 'ろ'] になるはず...
+                        // 「ち」の１文字目のローマ字を打った時に実行されるので、
+                        // ひとつの「ちゃ」のようなパターンに対して１回だけ実行される
+                        d.kana = candidateKana
+                        d.roman = candidateKanaData
+                        kanaRomanMap.splice(kanaIndex + 1, 1)
+                    }
+                }
+            }
+            const candidates = d.roman.filter(r => r.startsWith(`${romanInput}${e.key}`))
+            checkByCandidates(candidates, e.key)
         })
         const showRandomSentence = () => {
             sentence = sentences[Math.floor(Math.random() * sentences.length)]
             kanaRomanMap = sentence.kana.split('').map(k => ({kana: k, roman: romans[k]}))
             kanaIndex = 0
             romanInput = ''
+            inputForSentence = ''
             this.text_roman.text = kanaToRoman(sentence.kana)
             this.text_display.text = sentence.display
         }
@@ -330,6 +385,7 @@ export class ScenePlay extends Phaser.Scene {
             timer_task()
             timer_id = setInterval(timer_task, 1000)
         }
+        prepareForSmallChars()
         createTimer()
         startTimer()
         initTextArea()
