be235ff4d681505c4435c40cbf5c8b8f11fdcded
itc-s21009 <s21009@std.it-college.ac.jp>
2022-12-16 00:51:58 +0900

    テーブルクラスに線を引くようにした

--- src/custom_table.js
+++ src/custom_table.js
@@ -6,6 +6,7 @@ export default class CustomTable extends Phaser.GameObjects.Container {
 
     constructor(scene, x, y, W, H, data) {
         super(scene, x, y)
+        this.setSize(W, H)
         this.fontFamily = 'Arial'
         this.fontSize = 20
         if (data) {
@@ -17,13 +18,13 @@ export default class CustomTable extends Phaser.GameObjects.Container {
     setFontSize = (size) => {
         this.fontSize = size
         this.texts.forEach(t => t.setFontSize(size))
-        this.setTableData(this.tableData)
+        this.updateTable()
     }
 
     setFontFamily = (font) => {
         this.fontSize = font
         this.texts.forEach(t => t.setFontFamily(font))
-        this.setTableData(this.tableData)
+        this.updateTable()
     }
 
     setTableData = (tableData) => {
@@ -40,17 +41,28 @@ export default class CustomTable extends Phaser.GameObjects.Container {
         // スペースの横幅
         const spaceWidth = ctx.measureText('  ').width
         this.tableData.forEach(line => line.forEach((d, x) => maxWidths[x] = Math.max(ctx.measureText(`${d}`).width, maxWidths[x])))
+        const lineStartX = this.x - spaceWidth / 2
+        const lineEndX = maxWidths.reduce((a, b) => a + b) + spaceWidth * this.tableData[0].length
         this.texts = this.tableData.map((line, y) => {
             let offsetX = 0
+            const textY = this.y + y * (this.fontSize + 10)
+            if (y <= 1)  // 上から２本まで横の線を引く
+                this.scene.add.line(lineStartX, textY - this.fontSize / 4, 0, 0, lineEndX, 0, 0).setOrigin(0)
+            if (y === this.tableData.length - 1) // 一番下の線
+                this.scene.add.line(lineStartX, textY + (this.fontSize + 10) - this.fontSize / 4, 0, 0, lineEndX, 0, 0).setOrigin(0)
             line.map((d, x) => {
                 const textX = this.x + offsetX
-                const textY = this.y + y * (this.fontSize + 10)
                 offsetX += maxWidths[x] + spaceWidth
-                return this.scene.add.text(textX+(y === 0? maxWidths[x]/2:0), textY, d)
+                // 一番左端と、縦の線
+                this.scene.add.line(textX - spaceWidth / 2, textY - 5, 0, 0, 0, this.fontSize * 1.6, 0).setOrigin(0)
+                if (x === line.length - 1) {// 一番右端の線
+                    this.scene.add.line(this.x + offsetX - spaceWidth / 2, textY - 5, 0, 0, 0, this.fontSize * 1.6, 0).setOrigin(0)
+                }
+                return this.scene.add.text(textX + (y === 0 ? maxWidths[x] / 2 : 0), textY, d)
                     .setColor('black')
                     .setFontFamily(this.fontFamily)
                     .setFontSize(this.fontSize)
-                    .setOrigin(y === 0 ? 0.5 : 0, 0.5)
+                    .setOrigin(y === 0 ? 0.5 : 0, 0)
             })
         })
     }
