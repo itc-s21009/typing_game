a931e8143ef363a2052fc15b75845be2f8a10683
itc-s21009 <s21009@std.it-college.ac.jp>
2023-01-02 16:16:42 +0900

    文章を取得して全部表示するまで

--- src/react_components/App.js
+++ src/react_components/App.js
@@ -1,15 +1,56 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const App = () => {
     const [page, setPage] = useState(1)
+    const [data, setData] = useState([])
+    const [dataOnTable, setDataOnTable] = useState([])
     const options = {
         'new': '新しい順',
         'old': '古い順',
         'long': '文字数が長い順',
         'short': '文字数が短い順',
     }
+    const API_URL = 'http://localhost:3000'
+    const fetchData = () => {
+        axios.get(`${API_URL}/api/sentences`, {withCredentials: true})
+            .then(r => r.data)
+            .then(d => setData(d))
+    }
+    useEffect(fetchData, [data, API_URL])
+    useEffect(() => {
+        setDataOnTable(data)
+    }, [data])
     const ShowOrderOptions = () => Object.keys(options).map(k => <option value={k}>{options[k]}</option>)
     const ShowLimitOptions = () => [10, 20, 30, 40, 50].map(k => <option value={k}>{k}</option>)
+    const ShowTableRows = () =>
+        dataOnTable.map(d =>
+            <tr className="align-middle">
+                <td style={{width: '50%'}}>{d.sentence}</td>
+                <td style={{width: '50%'}}>{d.kana}</td>
+                <td style={{minWidth: '74px'}}>
+                    <a href={`/admin/sentences/edit/${d.id}`} className="btn btn-outline-primary">編集</a>
+                </td>
+                <td style={{minWidth: '74px'}}>
+                    <a href="javascript:void(0)" className="del-link btn btn-outline-danger"
+                       data-id={d.id} data-sentence={d.sentence} data-kana={d.kana}>削除</a>
+                </td>
+            </tr>
+        )
+    const ShowTable = () =>
+        <table className="table table-hover">
+            <thead>
+            <tr>
+                <th>文章</th>
+                <th>かな</th>
+                <th>編集</th>
+                <th>削除</th>
+            </tr>
+            </thead>
+            <tbody>
+            <ShowTableRows/>
+            </tbody>
+        </table>
     return (
         <>
             <h2 className="ms-2">文章一覧</h2>
@@ -37,16 +78,7 @@ const App = () => {
                     <a className="btn btn-outline-primary" href="/admin/sentences/new">文章追加</a>
                 </div>
             </div>
-            <table className="table table-hover">
-                <thead>
-                <tr>
-                    <th>文章</th>
-                    <th>かな</th>
-                    <th>編集</th>
-                    <th>削除</th>
-                </tr>
-                </thead>
-            </table>
+            <ShowTable/>
             <script src="/js/sentences.js"></script>
         </>
     )
